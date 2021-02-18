a6caa966a7124a9d87428c637726f66ddc6de581
chenyjzn <eliseo.cyc@gmail.com>
2021-02-19 00:50:47 +0800

    Hello triangle

--- app/src/main/java/com/yuchen/opengl/MyGLRenderer.kt
+++ app/src/main/java/com/yuchen/opengl/MyGLRenderer.kt
@@ -9,16 +9,20 @@ import javax.microedition.khronos.opengles.GL10
 
 class MyGLRenderer : GLSurfaceView.Renderer {
 
+    lateinit var triangle: Triangle
+
     @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     override fun onSurfaceCreated(unused: GL10, config: EGLConfig) {
         // Set the background frame color
         GLES30.glClearColor(0.2f, 0.3f, 0.3f, 1.0f)
+        triangle = Triangle()
     }
 
     @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     override fun onDrawFrame(unused: GL10) {
         // Redraw background color
         GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT)
+        triangle.draw()
     }
 
     override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
---
+++ app/src/main/java/com/yuchen/opengl/Triangle.kt
@@ -0,0 +1,108 @@
+package com.yuchen.opengl
+
+import android.opengl.GLES30
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
+import java.nio.IntBuffer
+
+@RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+class Triangle {
+
+    private val vaoIds = IntArray(1)
+    private val vboIds = IntArray(1)
+    private val eboIds = IntArray(1)
+
+    private val vertices = floatArrayOf(
+            0.5f,  0.5f, 0.0f,  // top right
+            0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f   // top left
+    )
+
+    private val indices = intArrayOf(
+            0, 1, 3,
+            1,2,3
+    )
+
+    private var indicesBuffer: IntBuffer =
+            ByteBuffer.allocateDirect(indices.size * Int.SIZE_BYTES).run {
+                order(ByteOrder.nativeOrder())
+                asIntBuffer().apply {
+                    put(indices)
+                    position(0)
+                }
+            }
+
+    private var vertexBuffer: FloatBuffer =
+        ByteBuffer.allocateDirect(vertices.size * Float.SIZE_BYTES).run {
+            order(ByteOrder.nativeOrder())
+            asFloatBuffer().apply {
+                put(vertices)
+                position(0)
+            }
+        }
+
+    private val vertexShaderCode =
+        "#version 300 es\n" +
+                "layout (location = 0) in vec3 aPos;\n" +
+                "void main() {\n" +
+                "     gl_Position = vec4(aPos, 1.0);\n" +
+                "}"
+    private val fragmentShaderCode =
+        "#version 300 es\n" +
+                "precision mediump float;\n" +
+                "out vec4 fragColor;\n" +
+                "void main() {\n" +
+                "     fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n" +
+                "}"
+
+    private var mProgram: Int
+
+    init {
+        val vertexShader: Int = GLES30.glCreateShader(GLES30.GL_VERTEX_SHADER).also { shader ->
+            GLES30.glShaderSource(shader, vertexShaderCode)
+            GLES30.glCompileShader(shader)
+        }
+        val fragmentShader: Int = GLES30.glCreateShader(GLES30.GL_FRAGMENT_SHADER).also { shader ->
+            GLES30.glShaderSource(shader, fragmentShaderCode)
+            GLES30.glCompileShader(shader)
+        }
+
+        mProgram = GLES30.glCreateProgram()
+        GLES30.glAttachShader(mProgram, vertexShader)
+        GLES30.glAttachShader(mProgram, fragmentShader)
+        GLES30.glLinkProgram(mProgram)
+
+        //VAO
+        GLES30.glGenVertexArrays(1, vaoIds, 0)
+        GLES30.glBindVertexArray(vaoIds[0])
+
+        //VBO
+        GLES30.glGenBuffers(1, vboIds, 0)
+        GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, vboIds[0])
+        GLES30.glBufferData(GLES30.GL_ARRAY_BUFFER, vertices.size * Float.SIZE_BYTES, vertexBuffer, GLES30.GL_STATIC_DRAW)
+
+        GLES30.glVertexAttribPointer(0, 3, GLES30.GL_FLOAT, false, 3 * Float.SIZE_BYTES, 0)
+        GLES30.glEnableVertexAttribArray(0)
+
+        //EBO
+        GLES30.glGenBuffers(1, eboIds, 0)
+        GLES30.glBindBuffer(GLES30.GL_ELEMENT_ARRAY_BUFFER,eboIds[0])
+        GLES30.glBufferData(GLES30.GL_ELEMENT_ARRAY_BUFFER, indices.size * Int.SIZE_BYTES, indicesBuffer, GLES30.GL_STATIC_DRAW)
+
+        GLES30.glBindVertexArray(0)
+    }
+
+    fun draw() {
+        GLES30.glUseProgram(mProgram)
+
+        GLES30.glBindVertexArray(vaoIds[0])
+
+        GLES30.glDrawElements(GLES30.GL_TRIANGLES, 6, GLES30.GL_UNSIGNED_INT, 0)
+
+        GLES30.glBindVertexArray(0)
+    }
+}
