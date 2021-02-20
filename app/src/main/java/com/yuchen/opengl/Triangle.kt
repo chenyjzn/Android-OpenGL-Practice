2f71d30f36117c02caf269935abd18a87c243d6e
chenyjzn <eliseo.cyc@gmail.com>
2021-02-20 23:15:39 +0800

    Shaders

--- app/src/main/java/com/yuchen/opengl/Triangle.kt
+++ app/src/main/java/com/yuchen/opengl/Triangle.kt
@@ -16,15 +16,14 @@ class Triangle {
     private val eboIds = IntArray(1)
 
     private val vertices = floatArrayOf(
-            0.5f,  0.5f, 0.0f,  // top right
-            0.5f, -0.5f, 0.0f,  // bottom right
-            -0.5f, -0.5f, 0.0f,  // bottom left
-            -0.5f,  0.5f, 0.0f   // top left
+            // positions         // colors
+            0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+            -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+            0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top
     )
 
     private val indices = intArrayOf(
-            0, 1, 3,
-            1,2,3
+            0, 1, 2,
     )
 
     private var indicesBuffer: IntBuffer =
@@ -48,15 +47,19 @@ class Triangle {
     private val vertexShaderCode =
         "#version 300 es\n" +
                 "layout (location = 0) in vec3 aPos;\n" +
+                "layout (location = 1) in vec3 aColor;\n" +
+                "out vec3 ourColor;\n"+
                 "void main() {\n" +
                 "     gl_Position = vec4(aPos, 1.0);\n" +
+                "     ourColor = aColor;\n" +
                 "}"
     private val fragmentShaderCode =
         "#version 300 es\n" +
                 "precision mediump float;\n" +
+                "in vec3 ourColor;\n"+
                 "out vec4 fragColor;\n" +
                 "void main() {\n" +
-                "     fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n" +
+                "     fragColor = vec4(ourColor,0.0);\n" +
                 "}"
 
     private var mProgram: Int
@@ -85,12 +88,15 @@ class Triangle {
         GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, vboIds[0])
         GLES30.glBufferData(GLES30.GL_ARRAY_BUFFER, vertices.size * Float.SIZE_BYTES, vertexBuffer, GLES30.GL_STATIC_DRAW)
 
-        GLES30.glVertexAttribPointer(0, 3, GLES30.GL_FLOAT, false, 3 * Float.SIZE_BYTES, 0)
+        GLES30.glVertexAttribPointer(0, 3, GLES30.GL_FLOAT, false, 6 * Float.SIZE_BYTES, 0)
         GLES30.glEnableVertexAttribArray(0)
+
+        GLES30.glVertexAttribPointer(1, 3, GLES30.GL_FLOAT, false, 6 * Float.SIZE_BYTES, 3 * Float.SIZE_BYTES)
+        GLES30.glEnableVertexAttribArray(1)
 
         //EBO
         GLES30.glGenBuffers(1, eboIds, 0)
-        GLES30.glBindBuffer(GLES30.GL_ELEMENT_ARRAY_BUFFER,eboIds[0])
+        GLES30.glBindBuffer(GLES30.GL_ELEMENT_ARRAY_BUFFER, eboIds[0])
         GLES30.glBufferData(GLES30.GL_ELEMENT_ARRAY_BUFFER, indices.size * Int.SIZE_BYTES, indicesBuffer, GLES30.GL_STATIC_DRAW)
 
         GLES30.glBindVertexArray(0)
@@ -101,7 +107,7 @@ class Triangle {
 
         GLES30.glBindVertexArray(vaoIds[0])
 
-        GLES30.glDrawElements(GLES30.GL_TRIANGLES, 6, GLES30.GL_UNSIGNED_INT, 0)
+        GLES30.glDrawElements(GLES30.GL_TRIANGLES, 3, GLES30.GL_UNSIGNED_INT, 0)
 
         GLES30.glBindVertexArray(0)
     }
